e75378c3553fed2183e3dcb831b5a52a0dbc872b
Tatyana Doktorova <31245439+tadoktor@users.noreply.github.com>
2019-01-21 13:14:13 +0100

    Update Step_3B1_TGG_retrieval_clean_by1_HCC_rat_GTX.R

--- Step_3B1_TGG_retrieval_clean_by1_HCC_rat_GTX.R
+++ Step_3B1_TGG_retrieval_clean_by1_HCC_rat_GTX.R
@@ -83,7 +83,7 @@ write.table(TGG_in_HCC_list, paste(Root_location,TGGinHCC_compoundset,sep=''),se
 
 ###Selecting the sample IDs for the HCC compounds in TGG
 HCC_samples_in_TGG <- unique(merge(newcat2, Samples, by.x ="TGG_compoundName", by.y = "compoundName" ))
-HCC_in_TGG_sampleset <- "/OutputFiles/TGG_in_HCC_samples.txt"
+HCC_in_TGG_sampleset <- "/OutputFiles/TGG_in_HCC_samples_GTX.txt"
 write.table(HCC_samples_in_TGG[,c(1:11)], paste(Root_location,HCC_in_TGG_sampleset,sep=''),sep="\t",row.names=F)
 
 sampleIDs <- unique(HCC_samples_in_TGG$sampleId)
@@ -139,7 +139,7 @@ while (counts <= length(sampleIDs_rat)){#IDs_initial) {
 }
 
 #rat_dataset1 <- "/OutputFiles/TGG_data_absolute_rat_HCC.txt"
-rat_dataset2 <- "/OutputFiles/TGG_data_log2fold_HCC_rat.txt"
+rat_dataset2 <- "/OutputFiles/TGG_data_log2fold_HCC_rat_GTX.txt"
 #plain_results_absolute_rat$geneSymbols <- unlist(as.character(plain_results_absolute_rat$geneSymbols))
 plain_results_log2fold_rat$geneSymbols <- unlist(as.character(plain_results_log2fold_rat$geneSymbols))
 #write.table(plain_results_absolute_rat, paste(Root_location,rat_dataset1,sep=''),sep="\t",row.names=F)
